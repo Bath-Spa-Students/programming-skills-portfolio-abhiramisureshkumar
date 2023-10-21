8ff56568d3aea46bcebfac8b0096bb9696803103
abhiramisureshkumar <abhiramis988@gmail.com>
2023-10-21 19:52:13 +0530

    Exercise2.py

--- Chapter 2- Variables & Comments/Exercises/Exercise2.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -1,3 +1,4 @@
 #Find a quote from a famous person you admire. Print the quote and the name of its author.
+
 quote= ("Eleanor roosevelt once said ," "'the future belongs to those who believe in their dreams.'" )
 print(quote)
