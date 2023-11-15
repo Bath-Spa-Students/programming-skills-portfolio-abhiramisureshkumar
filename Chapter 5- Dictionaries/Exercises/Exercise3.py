b93ac43bbf68ec8497099996e73fcaca1afe4d45
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-15 23:20:32 +0530

    Exercise3.py

--- Chapter 5- Dictionaries/Exercises/Exercise3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -4,3 +4,14 @@
 
 #to your glossary.When you run your program again, these new words and meanings should automatically be included in the output.
 
+glossary = {
+    "string": "A series of characters.",
+    "comment": "A note in a program that the Python interpreter ignores.",
+    "list": "A collection of items in a particular order.",
+    "if"  : "if statement evaluates condition",
+    "elif": "used to test multiple condition",
+    "else": "used to execute both the true part and the false part of a given condition",
+
+    }
+for word,meaning in glossary.items():
+    print(f"{word}:\n{meaning}\n")
