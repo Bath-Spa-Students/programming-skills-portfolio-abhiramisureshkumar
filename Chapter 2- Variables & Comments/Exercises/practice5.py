7c8675ffe7aa5725d8a6bfcfaed753ae215f112b
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 18:06:04 +0530

    practise5.py

--- Chapter 2- Variables & Comments/Exercises/practice5.py
+++ Chapter 2- Variables & Comments/Exercises/practice5.py
@@ -1,6 +1,8 @@
-#printing datatypes
-
-x=45
-y="bathspa"
-print(type(x))
-print(type(y))
+#List
+universities=["Bathspa","westlondon","stirling"]
+x=universities[0]
+y=universities[1]
+z=universities[2]
+print(x)
+print(y)
+print(z)
