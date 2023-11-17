2f0b0709abedcbbdfca3493a7117e4a3c1994e4b
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 19:58:10 +0530

    Practice5.py

---
+++ Chapter 4- Control Flow/Exercises/Practice5.py
@@ -0,0 +1,16 @@
+#Month and Seasons
+
+summer = ["June","July","August"]
+spring = ["March","April","May"]
+Autumn = ["September","October","November"]
+Winter = ["December","January","Fevruary"]
+
+month = input("Enter the month : ")
+if month in spring:
+    print("wow! it's the spring season ")
+elif month in summer:
+    print("wow! it's the summer season ")
+elif month in Autumn:
+    print("wow! it's the autumn season ")
+else:
+    print("wow! it's the winter season ")
