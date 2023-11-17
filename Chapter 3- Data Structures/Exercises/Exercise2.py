11e918726e46afaf66bf4b15eb9b5f4885bbde0b
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 18:15:36 +0530

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,5 @@
+names=["anagha","alex","riya","swetha"]
+print(f"{names[0]} is one of my friend")
+print(f"{names[1]} is one of my friend")
+print(f"{names[2]} is one of my friend")
+print(f"{names[3]} is one of my friend")
