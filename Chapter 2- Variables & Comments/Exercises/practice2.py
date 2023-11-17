ad452f48b74226621fb044f8e4e384c8db71bbb4
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 17:49:19 +0530

    practice2.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice2.py
@@ -0,0 +1,12 @@
+#average and percentage
+
+
+A=float(input("enter the marks of DVD: "))
+B=float(input("enter the marks of DSD: "))
+C=float(input("enter the marks of CODELAB: "))
+sum=A+B+C
+avg=sum/3
+print("the avg marks are",avg)
+total_marks=float(input("enter the total marks: "))
+percentage= sum/total_marks*100
+print("your total percentage is " ,percentage)
