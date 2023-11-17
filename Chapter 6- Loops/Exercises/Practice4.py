21ac3605aa24725c8a62a65ca8e07ed6ea7032fa
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 21:34:07 +0530

    Practice5.py

---
+++ Chapter 6- Loops/Exercises/Practice4.py
@@ -0,0 +1,7 @@
+#Break
+
+names = ["alexa","susan","sara"]
+for name in names:
+    print(name)
+    if name=="sara":
+        break
---
+++ Chapter 6- Loops/Exercises/Practice5.py
@@ -0,0 +1,12 @@
+#Largest number
+
+num_list = []
+while True:
+    num = float(input("enter the number(0 to exit): "))
+    num_list.append(num)
+    max_num= 0
+    min_num= 0
+    if num ==0:
+        break
+    print("the maximum number is : ",max(num_list))
+    print("the minimum number is : ",min(num_list))
