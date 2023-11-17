95f55e669fdbd8f7e62753c942041a2ebf65c106
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 20:28:46 +0530

    Exercise5.py

--- Chapter 5- Dictionaries/Exercises/Exercise5.py
+++ Chapter 5- Dictionaries/Exercises/Exercise5.py
@@ -3,14 +3,16 @@
 #owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and asyou do, print everything you know about 
 
 #each pet
+pets=[]
+pet={'kind': 'Dog','name':'jimmy','owner': 'Alexa','place':'USA'}
+pets.append(pet)
+pet= {'kind': 'Cat','name':'sissy','owner': 'Philips','place':'UAE'}
+pets.append(pet)
+pet={'kind': 'Birds','name':'tito','owner': 'George','place':'London'}
+pets.append(pet)
+    
 
-my_pets=[
-    {"kind": "Dog","owner": "Alexa"},
-    {"kind": "Cat","owner": "Philips"},
-    {"kind": "Birds","owner": "George"},
-    {"kind": "Fish","owner": "Aviona"},
-]
-
-for pet in my_pets:
-    print(f"kind of animal: {pet['kind']}\n")
-    print(f"owner's name:{pet ['owner']}\n")
+for pet in pets:
+    print("\nthis is about "+ pet['name'].title() +":")
+    for key, value in pet.items():
+      print("\t" + key + ":"+ str(value))
