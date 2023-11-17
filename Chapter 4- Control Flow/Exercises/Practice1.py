0270896118fd960c3e1471e6c6d74bbff5232529
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 19:38:12 +0530

    practice1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice1.py
@@ -0,0 +1,6 @@
+x=60
+if x > 40:
+    y = 20
+    z = 35
+    print("the value of y is ",y)
+    print ("the value of z is ",z)
---
+++ Chapter 4- Control Flow/Exercises/Practice2.py
@@ -0,0 +1,9 @@
+#If-else statement
+
+
+a = 18
+if a < 10:
+    b=9
+else:
+    b=30
+print("the value of b is ",b)
