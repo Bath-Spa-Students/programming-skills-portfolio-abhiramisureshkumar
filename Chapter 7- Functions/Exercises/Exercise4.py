396fd888fb970fdc69b6121bd60b97fc8ec073da
abhiramisureshkumar <abhiramis988@gmail.com>
2023-11-17 22:21:11 +0530

    Practice1.py

--- Chapter 7- Functions/Exercises/Exercise4.py
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -2,11 +2,10 @@
 
 #medium shirt with the default message, and a shirt of any size with a different message.
 
-def make_shirt(size="large",message="i love python"):
-    print(f"creating a {size} shirt with the message:{message}")
+def make_shirt(size='large',message='i love python'):
+    print("\ncreating a "+ size + "t-shirt")
+    print(' shirt with the message: '+ message)
      
-    make_shirt()
-
-    make_shirt(size="medium")
-    
-    make_shirt(size="small",message="python is the best programming language")
+make_shirt()
+make_shirt(size='medium')
+make_shirt(size='small',message='python is the best programming language')
---
+++ Chapter 7- Functions/Exercises/Practice1.py
@@ -0,0 +1,6 @@
+#Sum of two numbers
+
+def sum(a , b):
+    return a + b
+print (sum(6,5))
+print(sum(2,89.2))
